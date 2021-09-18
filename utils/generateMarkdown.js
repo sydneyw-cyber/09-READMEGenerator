185aea2100e3655468ee676ef84ab6c8ab2426bc
sydneyw-cyber <sydneyw521@gmail.com>
2021-09-18 03:15:10 -0500

    creating licenses links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,6 +32,16 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+function renderLicenseSection(license) {
+  const link = renderLicenseLink(license)
+  const licenseSection = `This application uses a license from ${license} 
+  \n * Link: ${link}`
+  if (license === 'None'){
+    return ''
+  } else {
+    return licenseSection
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 
